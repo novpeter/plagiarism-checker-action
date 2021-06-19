86c1bcfe0605b0ca1e5e0e2e2fcd27003ccf226b
Petr Novoselov <novpeter@yandex.ru>
2021-06-19 23:33:49 +0300

    Complete entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,5 +10,16 @@ chmod +x sherlock
 # Run python script to download all pull requests
 python3 main.py "${GITHUB_ACCESS_TOKEN}" "${REPOSITORY_NAME}"
 
-# Launch sherlock for repositories
-./sherlock -e swift $(echo ./repositories/*)
+# Remove all not .swift files
+find ./solutions/ -not -name "*.swift" -type f -delete
+
+for D in $(find ./solutions -mindepth 1 -maxdepth 1 -type d)
+do
+    # Make every subdirectory flatten
+    find "${D}" -mindepth 1 -type f -print -exec mv {} "${D}" \;
+    # Remove folders from solutions
+    find "${D}" -mindepth 1 -type d -exec rm -rf {} \;
+done
+
+# Launch sherlock for solutions
+cd solutions ; ../sherlock -e swift $(echo ./*) > ../outputs/result.txt
--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 import sys
+
 from github import Github
+from git import Repo
 
 ACCESS_TOKEN = sys.argv[1]
 REPOSITORY_NAME = sys.argv[2]
 
-if __name__ == '__main__':
-    g = Github(ACCESS_TOKEN)
-    repo = g.get_repo(REPOSITORY_NAME)
+# GitHub client
+github_client = Github(ACCESS_TOKEN)
+
+# Project repository
+repo = github_client.get_repo(REPOSITORY_NAME)
+
+
+def download_pulls():
+    """
+    Downloads pull requests in solutions folder. After execution all
+    solutions will contain only .swift files
+    """
     pulls = repo.get_pulls(state='open', sort='created', base='master')
     for pr in pulls:
-        info = f"user: {pr.user.name}, link: {pr.head.repo.clone_url}\n"
-        print(info)
+        author = pr.user.name
+        clone_url = pr.head.repo.clone_url
+        directory = f"./solutions/{author}"
+        Repo.clone_from(clone_url, directory)
+
+
+if __name__ == '__main__':
+    download_pulls()
